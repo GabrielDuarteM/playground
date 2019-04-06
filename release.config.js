758ffc68fac1597226555df196027dc932038a64
Gabriel Duarte <gabrielqduarte@gmail.com>
2019-04-06 20:29:58 +0200

    fix: release chrome version

--- release.config.js
+++ release.config.js
@@ -1,1 +1,24 @@
-module.exports = require("./semantic-release/semantic-release");
+module.exports = {
+  verifyConditions: ["semantic-release-chrome", "@semantic-release/github"],
+  prepare: [
+    {
+      path: "semantic-release-chrome",
+      asset: "playground.zip"
+    }
+  ],
+  publish: [
+    {
+      path: "semantic-release-chrome",
+      asset: "playground.zip",
+      extensionId: "almjngcniaaecgaaanfjpiieeaoimiif"
+    },
+    {
+      path: "@semantic-release/github",
+      assets: [
+        {
+          path: "playground.zip"
+        }
+      ]
+    }
+  ]
+};
